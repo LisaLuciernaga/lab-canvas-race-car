55ab9da8f7a4866c79a07f23fed242b3647b0e11
Lisa Schwetlick <schwetlick.lisa@gmail.com>
2023-04-30 16:26:02 +0200

    car can now be moved with arrow keys

--- js/index.js
+++ js/index.js
@@ -3,5 +3,45 @@ window.onload = () => {
     startGame();
   };
 
-  function startGame() {}
+  //<canvas id="canvas" width="500" height="700"></canvas>
+  //W3 flappy bird tutorial https://www.w3schools.com/graphics/game_intro.asp
+
+  let canvas =  document.getElementById("canvas");
+  let ctx = canvas.getContext("2d");
+
+  let image = document.createElement("img");
+    image.src = "images/road.png";
+    image.alt = "road";
+
+  let carImage = document.createElement("img");
+    carImage.src = "images/car.png";
+    carImage.alt = "car";
+
+  const car = {
+    image: carImage,
+    x: 215
+  }
+
+  function startGame() {
+    this.intervalId = setInterval(function (){
+      ctx.clearRect(0, 0, 500, 700);
+      ctx.drawImage(image, 0, 0, 500, 800);
+      ctx.drawImage(carImage, car.x, 450, 70, 140);
+    }, 60);
+  }
+
+  document.addEventListener("keydown", function (e){
+    if(e.code == "ArrowLeft"){
+      if(car.x>30) car.x-=10;
+    };
+
+    if(e.code == "ArrowRight"){
+      if (car.x<405) car.x+=10;
+      console.log(car.x);
+    };
+    console.log(e.code);
+  })
 };
+
+// Make the car move 
+
